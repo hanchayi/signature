0cb9da9abb59003178dffb21a577c189d9f3b4e0
hanchayi <hanchayi@163.com>
2023-08-28 13:30:20 +0800

    init listener

--- src/index.js
+++ src/index.js
@@ -1,2 +1,36 @@
 import { a } from './test'
-console.log("hello", a)
+
+export default class Signature {
+  constructor(canvas) {
+    this.canvas = canvas
+    this.listeners = []
+    this.init()
+  }
+
+  init() {
+    this.initListeners()
+  }
+
+  initListeners() {
+    this.addEventListener('mousedown', () => {
+      console.log('mousedown')
+    })
+    this.addEventListener('mouseup', () => {
+      console.log('mouseup')
+    })
+  }
+
+  addEventListener(event, listener) {
+    this.canvas.addEventListener(event, listener)
+    this.listeners.push({
+      event,
+      listener
+    })
+  }
+
+  destroy() {
+    this.listeners.forEach(({ listener, event }) => {
+      this.canvas.removeEventListener(event, listener)
+    })
+  }
+}
